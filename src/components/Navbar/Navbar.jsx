7e67af4c0b953c404bb290374669b64134443b9d
Tanay's Macbook <user@Tanays-MacBook-Air.local>
2025-06-23 16:25:00 +0530

    Navbar will implemented

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search from '../../assets/search_icon.svg'
+import bell from '../../assets/bell_icon.svg'
+import profile from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>  
+      <div className="navbar-right">
+        <img src={search} alt="" className='icons'/>
+        <p>Children</p>
+        <img src={bell} alt="" className='icons'/>
+        <div className="navbar-profile">
+          <img src={profile} alt="" className='profile'/>
+          <img src={caret_icon} alt=""/>
+          <div className="dropdown">
+            <p>Sign out</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
